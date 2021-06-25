128ee6d895686ade4ca75009e759b7937549d808
grewer <grewer@grewer.cn>
2021-06-25 13:44:42 +0800

    first commit

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '~': './app',
+        },
+      },
+    ],
+  ],
+};
